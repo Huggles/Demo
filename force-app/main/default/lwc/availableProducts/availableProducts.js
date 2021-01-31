165f067c3e77b2e9d2e4fd0f7cfa671f2f203c17
Hugo <Hugo.van.Krimpen@appsolutely.nl>
2021-01-31 17:55:38 +0100

    Added container lwc

--- force-app/main/default/lwc/availableProducts/availableProducts.js
+++ force-app/main/default/lwc/availableProducts/availableProducts.js
@@ -5,7 +5,6 @@
 import {LightningElement, api, wire} from 'lwc';
 
 import getAvailableProducts from '@salesforce/apex/OrderViewController.getAvailableProducts';
-import {consoleLogAsJSON} from "c/lwcHelper";
 
 export default class AvailableProducts extends LightningElement {
 
@@ -15,15 +14,45 @@ export default class AvailableProducts extends LightningElement {
     @api recordId;
 
 
-    @wire(getAvailableProducts, {})
-    availableProducts({ error, data }) {
+    get availableProductColumns(){
+        const columns = [
+            { label: 'Name', fieldName: 'Name' },
+            { label: 'List Price', fieldName: 'UnitPrice', type: 'currency' },
+        ]
+        return columns;
+    }
+
+    /**
+     * Stores the available product data.
+     * @type {[]}
+     * @private
+     */
+    _availableProductData = [];
+
+
+    /**
+     * Gets the available product data in lightning datatable format.
+     * In this scenario, its equal to the actual data.
+     * Reloads the table when the data is updated.
+     * @type {[]}
+     * @private
+     */
+    get availableProductData(){
+        return this._availableProductData;
+    }
+
+
+    /**
+     * Retrieves the available products.
+     * @param error
+     * @param data
+     */
+    @wire(getAvailableProducts, {orderId : '$recordId'})
+    retrieveAvailableProducts({ error, data }) {
         if (data) {
-            consoleLogAsJSON(data);
-            this.record = data;
-            this.error = undefined;
+            this._availableProductData = data;
         } else if (error) {
-            this.error = error;
-            this.record = undefined;
+            console.log(error);
         }
     }
 }
---
+++ force-app/main/default/lwc/orderView/orderView.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Hugo on 31/01/2021.
+ */
+
+import {api, LightningElement} from 'lwc';
+
+export default class OrderView extends LightningElement {
+
+    /**
+     * The order record id
+     */
+    @api recordId;
+
+
+}
