c9b81b5a011f0060d21bffbfe57ccad85f2051c3
Hugo <Hugo.van.Krimpen@appsolutely.nl>
2021-01-31 15:49:38 +0100

    Initial Commit

--- force-app/main/default/lwc/availableProducts/availableProducts.js
+++ force-app/main/default/lwc/availableProducts/availableProducts.js
@@ -2,8 +2,28 @@
  * Created by Hugo on 31/01/2021.
  */
 
-import {LightningElement} from 'lwc';
+import {LightningElement, api, wire} from 'lwc';
+
+import getAvailableProducts from '@salesforce/apex/OrderViewController.getAvailableProducts';
+import {consoleLogAsJSON} from "c/lwcHelper";
 
 export default class AvailableProducts extends LightningElement {
 
+    /**
+     * The order record id
+     */
+    @api recordId;
+
+
+    @wire(getAvailableProducts, {})
+    availableProducts({ error, data }) {
+        if (data) {
+            consoleLogAsJSON(data);
+            this.record = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.record = undefined;
+        }
+    }
 }
